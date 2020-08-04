24ac82ea38c351d256f9d48e15fd8b6649b5f205
AnaChamba <abm.chamba@gmail.com>
2020-08-03 23:30:03 -0500

    ApplicationDbContext.cs public DbSet<Bodega> Bodegas {get; set; }

--- AfectacionMedioAmbiente.Web/Data/ApplicationDbContext.cs
+++ AfectacionMedioAmbiente.Web/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using AfectacionMedioAmbiente.Web.Models;
 
 namespace AfectacionMedioAmbiente.Web.Data
 {
@@ -12,5 +13,13 @@ namespace AfectacionMedioAmbiente.Web.Data
             : base(options)
         {
         }
+        public DbSet<Bodega> Bodegas  { get; set; }
+        public DbSet<Pantalon> Pantalones { get; set; }
+        public DbSet<Camiseta> Camisetas { get; set; }
+        public DbSet<Polin> Polines { get; set; }
+        public DbSet<Boxer> Boxers { get; set; }
+        public DbSet<Top> Topes { get; set; }
+        public DbSet<RopaFoto> RopaFotos { get; set; }
+        public DbSet<Persona> Personas { get; set; }
     }
 }
