0d5e24fe14b468ea21fc79c42925f9eb32d6c4dc
AnaChamba <abm.chamba@gmail.com>
2020-08-03 23:18:19 -0500

    Cracion de las clases: 1.  Bodega.cs 2.  Pantalon.cs 3.  Polin.cs 4.  Boxer.cs 5.  Top.cs 6.  Persona.cs 7.  Fabrica.cs 8.  Camiseta.cs 9.  Ropa.cs

---
+++ AfectacionMedioAmbiente.Web/Models/Fabrica.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AfectacionMedioAmbiente.Web.Models
+{
+    public class Fabrica
+    {
+        public Camiseta CrearCamiseta(string marca="cuello redondo", string modelo = "polo")
+        {
+            CrearCamiseta camiseta = new Camiseta(marca, modelo);
+            return camiseta;
+        }
+    }
+}
